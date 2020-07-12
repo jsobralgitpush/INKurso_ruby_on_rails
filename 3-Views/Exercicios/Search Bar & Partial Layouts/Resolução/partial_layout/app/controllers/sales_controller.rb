2455336cd3bd750f035dc92a85cd3804c2b20046
José Sobral <jose.sobral@poli.ufrj.br>
2020-07-12 19:40:11 -0300

    Views

---
+++ 3-Views/Exercicios/Search Bar & Partial Layouts/Resolução/partial_layout/app/controllers/sales_controller.rb
@@ -0,0 +1,30 @@
+class SalesController < ApplicationController
+  def index
+    @clients = Client.all
+  end
+
+  def new
+  
+  end
+
+  def search
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase
+      @second_paramater = Client.all.where("lower(name) LIKE :search", search: @parameter).ids
+      @results = Sale.all.where("client_id = :search", search: @second_paramater)  
+    end  
+  end
+
+  def create
+    @sale = Sale.new(post_params)
+
+    @sale.save
+    redirect_to action: "index"
+  end
+
+  private def post_params
+    params.require(:sale).permit(:amount, :client_id, :product_id)
+  end
+end
---
+++ 3-Views/Exercicios/Search Bar & Partial Layouts/Resolução/partial_layout/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sales#index'
+
+  resource :products
+  resource :clients
+  
+  get 'sales/new' => 'sales#new'
+  post 'sales/new' => 'sales#create'
+
+  get 'sales/search' => 'sales#search', :as => 'search_page'
+end
